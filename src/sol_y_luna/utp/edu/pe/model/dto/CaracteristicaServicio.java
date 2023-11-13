5483832ca07d0d77c4dbc875f4cbf54b3b53970e
Renzo Calderon <rzocx22@gmail.com>
2023-11-12 22:17:10 -0500

    clase caracteristicaServicio Agregado

--- src/sol_y_luna/utp/edu/pe/model/dto/CaracteristicaServicio.java
+++ src/sol_y_luna/utp/edu/pe/model/dto/CaracteristicaServicio.java
@@ -1,7 +1,7 @@
 
 package sol_y_luna.utp.edu.pe.model.dto;
 
-
+//Clase CaracteristicaServicio creado
 public class CaracteristicaServicio extends Caracteristica {
     
     private String nombreServicio;
